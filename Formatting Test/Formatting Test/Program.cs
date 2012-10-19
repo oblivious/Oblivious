f0c112cde7212fc892f95d5f47764d19a522cb50
Donal <obyrned81@gmail.com>
2012-10-19 15:09:13 +0100

    IFormatProvider and ICustomFormatter stuff added.

--- Formatting Test/Formatting Test/Program.cs
+++ Formatting Test/Formatting Test/Program.cs
@@ -4,13 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Globalization;
 using System.Numerics;
+using System.Threading;
 
 namespace Formatting_Test
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main()
+        { 
+            //
+            Console.WindowWidth = 100;
+            Console.WindowHeight = 40;
+
+            Console.WriteLine(CultureInfo.CurrentCulture);
+            Console.WriteLine(CultureInfo.CurrentUICulture);
+
+
+            byte byteValue = 124;
+            Console.WriteLine(String.Format(new BinaryFormatter(),
+                                            "{0} (binary: {0:B}) (octal: {0:O}) (hex: {0:H})", byteValue));
+
+            int intValue = 23045;
+            Console.WriteLine(String.Format(new BinaryFormatter(),
+                                            "{0} (binary: {0:B}) (octal: {0:O}) (hex: {0:H})", intValue));
+
+            ulong ulngValue = 31906574882;
+            Console.WriteLine(String.Format(new BinaryFormatter(),
+                                            "{0}\n   (binary: {0:B})\n   (octal: {0:O})\n   (hex: {0:H})",
+                                            ulngValue));
+
+            BigInteger bigIntValue = BigInteger.Multiply(Int64.MaxValue, 2);
+            Console.WriteLine(String.Format(new BinaryFormatter(),
+                                            "{0}\n   (binary: {0:B})\n   (octal: {0:O})\n   (hex: {0:H})",
+                                            bigIntValue));
+
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            long acctNumber;
+            double balance;
+            DayOfWeek wday;
+            string output;
+
+            acctNumber = 104254567890;
+            balance = 16.34;
+            wday = DayOfWeek.Monday;
+
+            output = String.Format(new AcctNumberFormat(),
+                                   "On {2}, the balance of account {0:H} was {1:C2}.",
+                                   acctNumber, balance, wday);
+            Console.WriteLine(output);
+
+            wday = DayOfWeek.Tuesday;
+            output = String.Format(new AcctNumberFormat(),
+                                   "On {2}, the balance of account {0:I} was {1:C2}.",
+                                   acctNumber, balance, wday);
+            Console.WriteLine(output);
         }
     }
 
@@ -89,8 +138,8 @@ namespace Formatting_Test
                     baseNumber = 2;
                     break;
                 case "O":
-                    break;
                     baseNumber = 8;
+                    break;
                 case "H":
                     baseNumber = 16;
                     break;
@@ -106,15 +155,104 @@ namespace Formatting_Test
             }
 
             string numericString = string.Empty;
+
+            //Calculate the minimum required number of characters to display a value to a certain base.
+            double numChars = Math.Log(256d, baseNumber);
+            int numCharsAsInt = (int)(numChars + 0.5d);
+
             for (int ctr = bytes.GetUpperBound(0); ctr >= bytes.GetLowerBound(0); ctr--)
             {
                 string byteString = Convert.ToString(bytes[ctr], baseNumber);
-                if (baseNumber == 2)
-                {
-                    byteString = new String('0', 8 - byteString.Length) + byteString;
-                }
-                else if (baseNumber == 8)
+                byteString = new String('0', numCharsAsInt - byteString.Length) + byteString;
+                numericString += byteString + " ";
             }
+            return numericString.Trim();
+        }
+
+        private string HandleOtherFormats(string format, object arg)
+        {
+            if (arg is IFormattable)
+            {
+                return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            }
+            else if (arg != null)
+            {
+                return arg.ToString();
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+    }
+
+    public class AcctNumberFormat : IFormatProvider, ICustomFormatter
+    {
+        private const int ACCT_LENGTH = 12;
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string Format(string fmt, object arg, IFormatProvider formatProvider)
+        {
+            if (arg.GetType() != typeof(Int64))
+            {
+                try
+                {
+                    return HandleOtherFormats(fmt, arg);
+                }
+                catch (FormatException e)
+                {
+                    throw new FormatException(String.Format("The format of '{0}' is invalid.", fmt), e);
+                }
+            }
+
+            string ufmt = fmt.ToUpper(CultureInfo.InvariantCulture);
+            if (!(ufmt == "H" || ufmt == "I"))
+            {
+                try
+                {
+                    return HandleOtherFormats(fmt, arg);
+                }
+                catch (FormatException e)
+                {
+                    throw new FormatException(String.Format("The format of '{0}' is invalid.", fmt), e);
+                }
+            }
+
+            string result = arg.ToString();
+
+            if (result.Length < ACCT_LENGTH)
+            {
+                result = result.PadLeft(ACCT_LENGTH, '0');
+            }
+            if (result.Length > ACCT_LENGTH)
+            {
+                result = result.Substring(0, ACCT_LENGTH);
+            }
+            if (ufmt == "I")
+                return result;
+            else
+                return result.Substring(0, 5) + "-" + result.Substring(5, 3) + "-" + result.Substring(8);
+        }
+
+        private string HandleOtherFormats(string format, object arg)
+        {
+            if (arg is IFormattable)
+                return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            else if (arg != null)
+                return arg.ToString();
+            else
+                return string.Empty;
         }
     }
 }
