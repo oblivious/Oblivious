e9962eea4a6de10cb4e88216b26aea8caa87db10
Donal <obyrned81@gmail.com>
2012-11-05 14:23:54 +0000

    Some msdn stuff, async calls, events

--- EventsTest/EventsTest/Program.cs
+++ EventsTest/EventsTest/Program.cs
@@ -5,10 +5,85 @@ using System.Text;
 
 namespace EventsTest
 {
+    // From http://msdn.microsoft.com/en-us/library/w369ty8x%28v=vs.100%29.aspx
     class Program
     {
         static void Main(string[] args)
         {
+            Publisher pub = new Publisher();
+            Subscriber sub1 = new Subscriber("sub1", pub);
+            Subscriber sub2 = new Subscriber("sub2", pub);
+
+            // Call the method that raises the event.
+            pub.DoSomething();
+        }
+    }
+
+    public class CustomEventArgs : EventArgs
+    {
+        public CustomEventArgs(string s)
+        {
+            message = s;
+        }
+
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+    }
+
+    // Class that publishes an event
+    class Publisher
+    {
+        // Declare the event using EventHandler<T>
+        public event EventHandler<CustomEventArgs> RaiseCustomEvent;
+
+        public void DoSomething()
+        {
+            // Write some code that does somethign useful here
+            // then raise the event. You can also raise an event
+            // vefore you execute a block of code.
+            OnRaiseCustomEvent(new CustomEventArgs("Did something"));
+        }
+
+        // Wrap event invocations inside a protected virtual method
+        // to allow reived classes to override the event invocation behaviour
+        protected virtual void OnRaiseCustomEvent(CustomEventArgs e)
+        {
+            // Make a temporary copy of the event to avoid possibility of
+            // a race condition if the last suvscriver unsubscribes
+            // immediately after the null check and before the event is raised.
+            EventHandler<CustomEventArgs> handler = RaiseCustomEvent;
+
+            // Event will be null if there are no subscribers
+            if (handler != null)
+            {
+                // Format the string to send inside the CustomEventArgs parameter
+                e.Message += String.Format(" at {0}", DateTime.Now.ToString());
+
+                // Use the () operator to raise the event.
+                handler(this, e);
+            }
+        }
+    }
+
+    class Subscriber
+    {
+        private string id;
+        public Subscriber(string ID, Publisher pub)
+        {
+            id = ID;
+            // Subscribe to the event using C# 2.0 syntax
+            pub.RaiseCustomEvent += HandleCustomEvent;
+        }
+
+        // Define what actions to take whe the event is raised.
+        void HandleCustomEvent(object sender, CustomEventArgs e)
+        {
+            Console.WriteLine(id + " received this message: {0}", e.Message);
         }
     }
 }
