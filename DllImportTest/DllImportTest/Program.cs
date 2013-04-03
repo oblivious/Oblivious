d3652a6723af49972f47beaa3a681789b6f4f2bf
Donal <obyrned81@gmail.com>
2013-04-03 20:40:44 +0100

    Some commit

--- DllImportTest/DllImportTest/Program.cs
+++ DllImportTest/DllImportTest/Program.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace DllImportTest
 {
     class Program
     {
+        [DllImport("user32.dll", EntryPoint = "MessageBox")]
+        private static extern int ShowBox(IntPtr hWnd, string text, string caption, uint type);
+
         static void Main(string[] args)
         {
+            ShowBox(new IntPtr(0), "Hello, world!", "My box", 0);
         }
     }
 }
